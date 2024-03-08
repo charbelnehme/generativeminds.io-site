d7086d69b1005bfcce13448e50caba60e1e25d56
Charbel Nehme <charbel.nehme@outlook.com>
2024-03-08 19:06:49 +1100

    Add JS for notification functionality

---
+++ script.js
@@ -0,0 +1,4 @@
+function notifyMe() {
+    var email = document.getElementById('email').value;
+    alert("Thank you for subscribing! We will notify you at " + email + " once our site is ready.");
+}
